a3b73b44c4bbd820d65e09fbb6ab246a5a80ad59
Andrew Cho <cisdell@gmail.com>
2023-05-21 19:59:44 -0400

    need to revisit redirect hook. V6 is different.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
+import {useAuthContext} from './hooks/useAuthContext'
 //components
 import Home from "./pages/home/Home.js";
 import Login from "./pages/login/Login.js"
 import Signup from "./pages/signup/Signup.js"
 import Navbar from "./components/Navbar.js"
 
+
 //styles
 import "./index.css";
 
 function App() {
+  const {authIsReady, user} = useAuthContext()
+
   return (
     <div className="App">
+      {authIsReady && (
       <BrowserRouter>
       <Navbar/>
         <Routes>
+
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+
+          {/* 131 */}
         </Routes>
       </BrowserRouter>
+      )}
     </div>
   );
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
-import { createContext, useReducer } from "react";
-
+import { createContext, useReducer, useEffect } from "react";
+import {projectAuth} from '../firebase/config'
 export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
@@ -8,6 +8,8 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case "LOGOUT":
       return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return {...state, user: action.payload, authIsReady: true}
     default:
       return state;
   }
@@ -16,9 +18,18 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
-  });
-  console.log("AuthContext state:", state);
+    authIsReady: false
+  })
 
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged((user) => {
+      dispatch({type: 'AUTH_IS_READY', payload: user})
+      unsub()
+    })
+  }, [])
+
+  console.log("AuthContext state:", state);
+//129 check out again
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
