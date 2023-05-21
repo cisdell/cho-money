6b459278d4d8d2c2bb7744f463c5a26dcc1b8c36
Andrew Cho <cisdell@gmail.com>
2023-05-21 16:00:42 -0400

    logout complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
+import { useLogout } from '../hooks/useLogout'
 //styles
 import styles from "./Navbar.module.css";
 
 import React from "react";
 
 export default function Navbar() {
+  const {logout} = useLogout()
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Signup</Link></li>
+        <li>
+            <button className='btn' onClick={logout}>LOGOUT</button>
+        </li>
       </ul>
     </nav>
   );
