c5362314c7e69af2796ebb59be4366c6ffc5d13f
Andrew Cho <cisdell@gmail.com>
2023-05-21 21:23:09 -0400

    added redirects using react router dom v6

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
-import {useAuthContext} from './hooks/useAuthContext'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 //components
 import Home from "./pages/home/Home.js";
-import Login from "./pages/login/Login.js"
-import Signup from "./pages/signup/Signup.js"
-import Navbar from "./components/Navbar.js"
-
+import Login from "./pages/login/Login.js";
+import Signup from "./pages/signup/Signup.js";
+import Navbar from "./components/Navbar.js";
 
 //styles
 import "./index.css";
 
 function App() {
-  const {authIsReady, user} = useAuthContext()
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <div className="App">
       {authIsReady && (
-      <BrowserRouter>
-      <Navbar/>
-        <Routes>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/signup"
+              element={user ? <Navigate to="/home" /> : <Signup />}
+            />
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/home" /> : <Login />}
+            />
 
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+            <Route path="/redirect" element={<Navigate to="/home" />} />
 
-          {/* 131 */}
-        </Routes>
-      </BrowserRouter>
+            {/* 131 */}
+          </Routes>
+        </BrowserRouter>
       )}
     </div>
   );
