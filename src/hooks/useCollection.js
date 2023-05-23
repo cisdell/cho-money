f67843aac1351276cc54fad44349a30a64a48dd3
Andrew Cho <cisdell@gmail.com>
2023-05-23 17:23:41 -0400

    fixed bug so we can add query to the firebase collection

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,13 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { projectFirestore } from "../firebase/config";
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    let ref = projectFirestore.collection(collection);
+  // if we don't use a ref --> infinate loop in useEffect
+  // _query is an array and is "different" on every function call
+  const query = useRef(_query).current
 
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection)
+    if (query) {
+      ref = ref.where(...query)
+    }
     const unsubscribe = ref.onSnapshot(
       (snapshot) => {
         let results = [];
@@ -28,7 +34,7 @@ export const useCollection = (collection) => {
     // unsubscribe on unmount
 
     return () => unsubscribe();
-  }, [collection]);
+  }, [collection, query]);
 
   return { documents, error };
 };
