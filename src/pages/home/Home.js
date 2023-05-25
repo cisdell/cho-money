36c76bc124c72c5439b9382855cfb4974b6283dd
Andrew Cho <cisdell@gmail.com>
2023-05-25 13:03:02 -0400

    added created at query

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -12,7 +12,9 @@ export default function Home() {
   const { user } = useAuthContext();
   const { documents, error } = useCollection(
     "transactions",
-    ["uid","==",user.uid]);
+    ["uid","==",user.uid],
+    ["createdAt", "desc"]);
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
