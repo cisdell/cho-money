93ac73dec152c4e6b9b7b3f756ce39d2957d1340
Andrew Cho <cisdell@gmail.com>
2023-05-25 15:46:34 -0400

    changed delete doc type to null

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -15,7 +15,7 @@ const firestoreReducer = (state, action) => {
     case "ADDED_DOCUMENT":
       return { isPending: false, document: action.payload, success: true, error: null}
     case "DELETED_DOCUMENT":
-      return { isPending: false, document: action.payload, success: true, error: null}
+      return { isPending: false, document: null, success: true, error: null}
     case "ERROR":
       return { isPending: false, document: null, success: false, error: action.payload}
     default:
